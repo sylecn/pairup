3ff8d3244c034ab8e6f3adea2cb11a4d704472ae
Yuanle Song <sylecn@gmail.com>
2010-02-02 10:39:33 -0600

    continue working on use case and UI; add how_to and queries.sql

---
+++ docs/queries.sql
@@ -0,0 +1,11 @@
+-- -*- mode: sql -*-
+
+-- sql queries for common tasks.
+
+-- this can be implemented in server side script or using mysql Views.
+
+--=====================
+-- members in a ticket 
+--=====================
+
+select 
